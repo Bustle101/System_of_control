64e4e27ee6f4c643e94d199f278b8f7c3bffc629
Kate Ned <aborigen70672022@gmail.com>
2025-10-06 17:59:01 +0300

    Подключение к бд

--- control-pro/server/index.js
+++ control-pro/server/index.js
@@ -1,22 +1,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import projectRoutes from "./routes/projects.js";
 
-dotenv.config();
+dotenv.config({ path: "./server/.env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Тестовый маршрут
+// Подключаем маршруты
+app.use("/api/projects", projectRoutes);
+
+// Проверочный маршрут
 app.get("/", (req, res) => {
-  res.send("Сервер работает!");
+  res.send(" Сервер работает");
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(` Сервер запущен на порту ${PORT}`);
 });
